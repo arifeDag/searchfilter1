3625a31b10c9b4a976adcb78d11d5ec821bd3d89
Arife Kara <arifedagkara@gmail.com>
2022-12-16 00:08:07 +0300

    ödev 19mtamamlandı

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import React from "react";
 import "./App.css";
+import Table from "./Table";
+import { Users } from "./users";
 
 function App() {
-    return(
-        <div>
-            <input type="text" placeholder="Search" className="search"/>
-        </div>
-    )
+  const [query, setQuery] = React.useState("");
+  const keys =["first_name","last_name","email"]
+  const search =(data)=>{
+    return data.filter((item)=>
+    keys.some((key)=>item[key].toLowerCase().includes(query)));
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search"
+        className="search"
+        onChange={(e) => setQuery(e.target.value)}/>
+        <Table data={search(Users)}/>
+     
     
+    </div>
+  );
 }
 export default App;
